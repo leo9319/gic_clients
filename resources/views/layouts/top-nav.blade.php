dfe5f8ab8da0e376bb6f0f8d63b9e7924e72eeae
unknown <riaz.i3216@gmail.com>
2018-07-17 14:19:21 +0600

    adding image to gic navbar

--- resources/views/layouts/top-nav.blade.php
+++ resources/views/layouts/top-nav.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-default navbar-fixed-top">
    <div class="brand">
-      <a href="{{ route('main') }}"><img src="{{ asset('img/logo.png') }}" alt="GIC Logo" class="img-responsive logo"></a>
+      <a href="{{ route('main') }}"><img src="{{ asset('img/GIC-Logo.png') }}" alt="GIC Logo" class="img-responsive logo"></a>
    </div>
    <div class="container-fluid">
       <div class="navbar-btn">
